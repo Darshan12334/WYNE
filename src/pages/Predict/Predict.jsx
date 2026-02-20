5101a47f4bc94e4290120984d29ceb306b09748d
Darshan Singh Chauhan <dashiee@Darshans-MacBook-Air.local>
2026-02-20 19:41:03 +0530

    Updated version

--- src/pages/Predict/Predict.jsx
+++ src/pages/Predict/Predict.jsx
@@ -39,7 +39,7 @@ function Predict() {
     setConfidence(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://wyne-backend.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
